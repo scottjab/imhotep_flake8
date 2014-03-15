d3ebb0a4e4d58dc69f8d5acf76bb468776ff9b96
Justin Abrahms <justin@abrah.ms>
2014-03-15 13:25:12 -0700

    imhotep_flake8 requires flake8

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     author='James Scott',
     author_email='scottjab@gmail.com',
     description='An imhotep plugin for python validation',
+    install_requires=["flake8"],
     entry_points={
         'imhotep_linters': [
             '.py = imhotep_flake8.plugin:Flake8Linter'
