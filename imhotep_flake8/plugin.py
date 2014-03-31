8b684d25b32b8bc7c9355077fd179cdd4fb8da32
James Scott <jim.scott@urbanairship.com>
2014-03-31 10:29:29 -0700

    Small fixes.

--- imhotep_flake8/plugin.py
+++ imhotep_flake8/plugin.py
@@ -2,7 +2,9 @@ from imhotep.tools import Tool
 from collections import defaultdict
 import json
 import os
+import logging
 
+log = logging.getLogger(__name__)
 
 class Flake8Linter(Tool):
     def get_configs(self):
@@ -22,10 +24,14 @@ class Flake8Linter(Tool):
                 if '.py' in filename:
                     python_files.append("%s/%s" % (dirname, filename))
 
+            if len(python_files) == 0:
+                # Exit early if no files, will speed linting.
+                return retval
             cmd = "flake8 %s %s" % (config, " ".join(python_files))
         try:
             output = self.executor(cmd)
             for line in output.split('\n'):
+                log.debug("flake8: %s", line)
                 path, line, column, message = line.split(':')
                 file_name = os.path.abspath(path)
                 file_name = file_name.replace(dirname, "")[1:]
